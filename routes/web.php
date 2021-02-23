8a7fb1236a0c7a3a395c474f26e2a17fa4bd5cb3
Ayush kamacharya <40533133+ayus93@users.noreply.github.com>
2021-02-23 22:56:32 +0545

    setting login page and admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/login', 'AdminLoginController@adminLogin')->name('adminLogin');
+
+Route::get('/admin/dashboard', 'AdminLoginCOntroller@dashboard')->name('adminDashboard');
